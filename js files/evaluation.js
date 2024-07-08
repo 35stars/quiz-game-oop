9e956ae17d1cb4d421e5e5148146ed3d369b5dfb
Jamestar Navarro <122615373+35stars@users.noreply.github.com>
2024-07-08 15:12:56 +0800

    Update evaluation.js

--- js files/evaluation.js
+++ js files/evaluation.js
@@ -1,14 +1,14 @@
-import * as main  from "../js files/main.js";
+import * as main  from "./main.js";
 
 
 function perfect(){
-    new Audio('../sound efx/winfantasia-6912.mp3').play()
+    new Audio('sound efx/winfantasia-6912.mp3').play()
     main.quiz.showAnswers()
     main.innerResult.textContent = ` ${main.quiz.result}`
 }
 
 function passed(){
-    new Audio('../sound efx/winfantasia-6912.mp3').play()
+    new Audio('sound efx/winfantasia-6912.mp3').play()
     main.quiz.showAnswers()
     main.innerResult.textContent = ` ${main.quiz.result}!`
 }
